b338ea63d1a0e689b625f195f56ab75ea9e9bcb5
nishanthkulal <nishanthkulal65462@gmail.com>
2023-10-18 23:40:46 +0530

    banner,profile image,name,prof added

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View,Text,Image,StyleSheet } from "react-native";
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  <View style = {styles.rectangle}>
+    <Image source={require('./assets/img1.jpg')} style={styles.profimg} />
+    <Text style={styles.title} >Nishanth Kulal</Text>
+    <Text style={styles.profession} >React-Native Developer</Text>
+  </View>)
 
+}
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  rectangle : {
+   width:'100%',
+   height:180,
+   backgroundColor:'#E8D8BE',
+
   },
+  profimg : {
+    width : 100,
+    height:100,
+    borderRadius:75,
+    position : 'absolute',
+    top:100,
+    left:30,
+  },
+  title : {
+    fontSize : 30,
+    fontWeight : 'bold',
+    position : 'absolute',
+    top : 89,
+    left: 150,
+  },
+  profession :{
+    fontSize : 18,
+    position : 'absolute',
+    top : 130,
+    left: 160,
+    color: '#888888'
+  }
 });
+export default App;
